dc530b1550e0cc740cf87b453ff53a254c328757
dragoonzx <dragoonz@yandex.ru>
2019-05-04 00:49:22 +0300

    first commit

---
+++ client/script/slider.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  $('.slider').slick({
+    infinite: true,
+    autoplaySpeed: 2000,
+    autoplay: true,
+    slidesToShow: 4,
+    variableWidth: true
+  })
+})
---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+
+var http = require('http')
+
+var app = express()
+
+var port = 8000
+
+var httpServer = http.createServer(app)
+
+app.use(express.static(__dirname + '/client'))
+
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/client/index.html')
+})
+
+httpServer.listen(port, () => console.log('server is listening'))
